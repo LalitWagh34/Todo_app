ac8c3d6511a6b97179e6490b8ba526829cc67e2e
unknown <lalitwagh2804@gmail.com>
2024-02-03 20:36:43 +0530

    Added backend

--- BackEnd/Middleware/user.js
+++ BackEnd/Middleware/user.js
@@ -1,35 +1,43 @@
 const jwt = require("jsonwebtoken")
-const JWT_SECRET = require('../config')
+const JWT_SECRET= require('../config')
 const {User} = require('../DataBase/db')
 
 function authMiddleware(req , res ,next){
     const authHeaders = req.headers.authorization;
-    if (!authHeaders || !authHeaders.startsWith('Bearer ')){
-        return res.status(403).json({
-            msg:"Wrong Inputs I think"
-        })
-    }
-    const token = authHeaders.split('')[1];
     try{
-        const decoded = jwt.verify(token , JWT_SECRET) 
-        req.userId = decoded.userId
+        if (!authHeaders || !authHeaders.startsWith('Bearer ')){
+            return res.status(403).json({
+                msg:"Wrong Inputs I think"
+            })
+        }
+        const token = authHeaders.split(' ')[1];
+        const decoded = jwt.verify(token , JWT_SECRET);
+        if (decoded.username){
+            next();
+        }else{
+            res.status(403).json({
+                msg:"You  are not authorized"
+            })
+        }
     }
     catch(error){
-        return res.status(403).json({
-            msg:"Something went Wrong"
+        console.error(error)
+        res.json({
+            msg:"Incorrect Inputs"
         })
     }
+   
    
 
 }
 
 async function userMiddleware (req ,res ,next){
-    const username = req.username
+    const username = req.headers.username
     const userExist = await User.findOne({
         username
     })
     if (!userExist){
-        return res.json({
+        return res.status(403).json({
             msg:"Create a New Account to Continue"
         })
     }
--- BackEnd/Routes/Todos/Todos.js
+++ BackEnd/Routes/Todos/Todos.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { authMiddleware, userMiddleware } = require("../../Middleware/user");
-const { TodoSchema } = require("./Validate/zodvalidate");
+const { TodoSchema, UpdateSchema } = require("./Validate/zodvalidate");
 const { Todos, User } = require("../../DataBase/db");
 const router = express.Router()
 
@@ -26,8 +26,96 @@ router.post("/newTodo" ,authMiddleware,userMiddleware, async(req ,res)=>{
         }
     })
     res.json({
+        todo : todo ,
         id : todo._id,
-        Msg:"Todos updatedSuccessfully"
+        Msg:"Todos Added  Successfully"
     })
-} )
+})
+
+router.get("/myTodo" , authMiddleware , userMiddleware, ( req ,res) =>{
+    const userId = req.userId ;
+    Todos.find({ userId })
+    .then((todos) => {
+      res.json(todos);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to retrieve todos' });
+    });
+    
+})
+
+router.put("/updateTodo/:todoId" , authMiddleware ,userMiddleware , async(req , res) =>{
+    const todoId = req.params.todoId;
+    const todoExist = await Todos.find({
+        _id:todoId
+    })
+    if(!todoExist){
+        res.status(403).json({
+            msg:"Todo Does not Exists"
+        })
+    }
+    const success = UpdateSchema.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            msg:"Invalid type Entries are there"
+        })
+    }
+
+    const updateTodos = {
+        $set:{
+        }  
+    }
+    if (req.body.tag !== undefined) {
+        updateTodos.$set.tag = req.body.tag;
+    }
+    if (req.body.title !== undefined) {
+        updateTodos.$set.title = req.body.title;
+    }
+    if (req.body.description !== undefined) {
+        updateTodos.$set.description = req.body.description;
+    }
+    if (req.body.completed !== undefined) {
+        updateTodos.$set.completed = req.body.completed;
+    }
+    
+    var updatedTodo = await Todos.updateOne(
+        { _id: todoId },
+        updateTodos
+    )
+    res.json({
+        msg:"Upadated Successfully",
+        updatedTodo
+    })
+
+})
+
+router.delete("/deleteTodo/:todoId" , authMiddleware , userMiddleware ,async(req,res)=>{
+    const todoId = req.params.todoId;
+  
+        const todoExists = await Todos.findOne({
+            _id : todoId
+        })
+        if (!todoExists){
+            return res.json({
+                msg:"Nothing to delete,this Todo does not exist"
+            })
+        }
+        await Todos.deleteOne({
+           _id : todoId
+        })
+    
+        await User.updateOne(
+            {username : req.username},
+            {
+                "$pull":{
+                    Todos : todoId
+                }
+            }
+        )
+        res.json({ 
+            msg: "Todo deleted" 
+        })
+ 
+    
+})
 module.exports = router
